e6a9bf3cc84342019fc315efaf32e18607e3bd54
Pankaj <pankajkak@cybage.com>
2022-09-27 18:56:53 +0530

    User pojo/model added

--- BackEnd/FoodAppDemoGit/src/main/java/com/cybage/model/User.java
+++ BackEnd/FoodAppDemoGit/src/main/java/com/cybage/model/User.java
@@ -1,6 +1,107 @@
 package com.cybage.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name = "user")
 public class User {
+	@Id
+	@GeneratedValue
+	private int userId;
+	private String userName;
+	private String userPassword;
+	private String userEmail;
+	private String userMobileNo;
+	private int attemptsCount;
 
+
+
+//
+//	@JsonIgnore
+//	@OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+//	private List<UserOrder> userOrders;
+//
+//	@JsonIgnore
+//	@OneToMany(mappedBy = "user")
+//	private List<Feedback> feedbackList;
+//
+//	public User() {
+//		this.feedbackList = new ArrayList<>();
+//	}
+	
+	public User() {
+		
+	}
+
+	public User(int userId, String userName, String userPassword, String userEmail, String userMobileNo,
+			int attemptsCount) {
+		this.userId = userId;
+		this.userName = userName;
+		this.userPassword = userPassword;
+		this.userEmail = userEmail;
+		this.userMobileNo = userMobileNo;
+		this.attemptsCount = attemptsCount;
+		
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserMobileNo() {
+		return userMobileNo;
+	}
+
+	public void setUserMobileNo(String userMobileNo) {
+		this.userMobileNo = userMobileNo;
+	}
+
+	public int getAttemptsCount() {
+		return attemptsCount;
+	}
+
+	public void setAttemptsCount(int attemptsCount) {
+		this.attemptsCount = attemptsCount;
+	}
+
+
+	
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userPassword=" + userPassword + ", userEmail="
+				+ userEmail + ", userMobileNo=" + userMobileNo + ", attemptsCount=" + attemptsCount + "]";
+	}
 }
