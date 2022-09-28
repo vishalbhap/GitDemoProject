6b66b6bcbb973d1581903cfaf0b3db6ba1ddcb4f
vishal <vishalbhap@cybage.com>
2022-09-28 15:14:01 +0530

    application.properties added. Minor changes in user model and FoodAppDemo.java

--- BackEnd/FoodAppDemoGit/src/main/java/com/cybage/food/FoodAppDemoGitApplication.java
+++ BackEnd/FoodAppDemoGit/src/main/java/com/cybage/food/FoodAppDemoGitApplication.java
@@ -2,8 +2,10 @@ package com.cybage.food;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.cybage.model")
 public class FoodAppDemoGitApplication {
 
 	public static void main(String[] args) {
--- BackEnd/FoodAppDemoGit/src/main/java/com/cybage/model/User.java
+++ BackEnd/FoodAppDemoGit/src/main/java/com/cybage/model/User.java
@@ -17,21 +17,6 @@ public class User {
 	private String userEmail;
 	private String userMobileNo;
 	private int attemptsCount;
-
-
-
-//
-//	@JsonIgnore
-//	@OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-//	private List<UserOrder> userOrders;
-//
-//	@JsonIgnore
-//	@OneToMany(mappedBy = "user")
-//	private List<Feedback> feedbackList;
-//
-//	public User() {
-//		this.feedbackList = new ArrayList<>();
-//	}
 	
 	public User() {
 		System.out.println("In constructor");
@@ -44,64 +29,6 @@ public class User {
 		this.userPassword = userPassword;
 		this.userEmail = userEmail;
 		this.userMobileNo = userMobileNo;
-		this.attemptsCount = attemptsCount;
-		
-	}
-
-	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getUserPassword() {
-		return userPassword;
-	}
-
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
-	}
-
-	public String getUserEmail() {
-		return userEmail;
-	}
-
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-
-	public String getUserMobileNo() {
-		return userMobileNo;
-	}
-
-	public void setUserMobileNo(String userMobileNo) {
-		this.userMobileNo = userMobileNo;
-	}
-
-	public int getAttemptsCount() {
-		return attemptsCount;
-	}
-
-	public void setAttemptsCount(int attemptsCount) {
-		this.attemptsCount = attemptsCount;
-	}
-
-
-	
-
-	@Override
-	public String toString() {
-		return "User [userId=" + userId + ", userName=" + userName + ", userPassword=" + userPassword + ", userEmail="
-				+ userEmail + ", userMobileNo=" + userMobileNo + ", attemptsCount=" + attemptsCount + "]";
+		this.attemptsCount = attemptsCount;		
 	}
 }
